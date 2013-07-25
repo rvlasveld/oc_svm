f585418552b062678824c2868fdfe1c9f923b670
Roemer Vlasveld <roemer.vlasveld@gmail.com>
2013-07-25 10:26:40 +0200

    Preload data script

---
+++ preload_data.m
@@ -0,0 +1,19 @@
+% PRE-LOAD SOME ACCELEROMETER DATA
+%
+% This script pre-loads accelerometer data, into the variable 'data'.
+% Special segments are available in 'downstairs' and 'upstairs'. The
+% indices of that segments are in 'indices_downstairs' and
+% 'indices_upstairs'.
+% Some very ambiguous manually-chosen (activity ) change points are available
+% in 'cp_seconds'
+
+data = csvread('data/stand-downstairs-stand-upstairs-stand-downstairs-stand-upstairs-stand/20130404_150025/accelerometer.csv');
+data = data(:,2:5);
+
+indices_downstairs  = find(data(:,1) > 13 & data(:,1) < 23);
+indices_upstairs    = find(data(:,1) > 34 & data(:,1) < 43);
+
+downstairs          = data(indices_downstairs, 1:4);
+upstairs            = data(indices_upstairs, 1:4);
+
+cp_seconds          = [7 13 22 27.5 29.5 34 43 48 49.5 55 63 68.5 70.5 76 85 90.5 92 94];
