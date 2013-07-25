bb4abd78e5e70448a2c28df8ee9182df23354af2
Roemer Vlasveld <roemer.vlasveld@gmail.com>
2013-07-25 10:27:37 +0200

    Add function to draw a vector of vertical lines

---
+++ draw_verical_lines.m
@@ -0,0 +1,21 @@
+%DRAW_VERICAL_LINES Draw a series of vertical lines in the current figure
+%
+%       HANDLES = DRAW_VERTICAL_LINES(POSITIONS)
+%
+% Draw a series of vertical lines in the current (=last used) figure.
+% The values in POSITIONS correspond to the position in the x-axis at which
+% a line will be drawn.
+%
+% Currently, the properties of the line can not be set. The color is set to
+% black.
+
+function handles = draw_verical_lines( positions )
+
+    yL = get(gca, 'YLim');
+    handles = zeros(length(positions),1);
+    for i = 1 : length(positions)
+        handles(i) = line([positions(i) positions(i)], yL, 'Color', 'k');
+    end
+
+end
+
