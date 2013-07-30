d2a55fa0f5611b142be95fea9154bd80b22424f9
Roemer Vlasveld <roemer.vlasveld@gmail.com>
2013-07-30 16:06:16 +0200

    Fix bumps in data on change points

--- load_camci_data.m
+++ load_camci_data.m
@@ -9,37 +9,42 @@
 %   change in variance.
 
 function [datapoints, change_points] = load_camci_data( data_length, segment_size, change_type )
-    if nargin < 1
-        data_length = 1000;
-    end
-
-    if nargin < 2
-        segment_size = 100;
-    end
-
-    if nargin < 3
-        change_type = 1;
-    end
+    
+    if nargin < 1; data_length  = 1000; end
+    if nargin < 2; segment_size = 100; end
+    if nargin < 3; change_type  = 1; end
     
 
-    x = [1 2];
+    x = [0 0];
     
     mean = 0;
     variance = 1;
     for i = 3:data_length
         [mean, variance] = mean_and_var(i, data_length, segment_size, change_type, mean, variance);
-        x(i) = 0.6*x(i-1) - 0.5*x(i-2) + normrnd(mean,variance);
+        
+        mod_i = mod(1, segment_size);
+        new_value = normrnd(mean,variance);
+        
+        if mod_i == 1 || mod_i == 2
+            x(i) = new_value;
+        else
+            x(i) = 0.6*x(i-1) - 0.5*x(i-2) + new_value;
+        end
+        
+        fprintf( '=== Value for %i : %f, mean: %f, variance: %f === \n\n', i, x(i), mean, variance);
     end
     
     datapoints = x;
-    change_points = 1 : segment_size : data_length;
+    change_points = 1 + segment_size : segment_size : data_length;
 end
 
-
 function [mean, variance] = mean_and_var(i, data_length, segment_size, type, mean, variance)
-
+    
     if mod(i, segment_size) == 0
         y = i / segment_size;
+        
+        fprintf( '\n \n \n Switching for i: %i , y: %f \n', i, y );
+        
         switch type
             case 1
                 % Change in mean
@@ -57,12 +62,18 @@ function [mean, variance] = mean_and_var(i, data_length, segment_size, type, mea
                 end
                 
         end
+        
+        fprintf( 'Mean: %f, variance: %f \n', mean, variance );
     end
     
     if type == 2
+        
         % Change in mean and variance. Mean already done in switch block
         variance = 0.1/(0.01 + (data_length - i)/data_length);
+        fprintf( 'Variance: %f \n', variance );
     end
+    
+    fprintf( 'Mean: %f \n', mean );
     
 end
 
