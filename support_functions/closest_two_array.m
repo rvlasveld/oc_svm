f657c0f111def7767a8deb391b59039a1b1318cb
Roemer Vlasveld <roemer.vlasveld@gmail.com>
2013-12-11 17:15:48 +0100

    Add original change points to result

--- support_functions/closest_two_array.m
+++ support_functions/closest_two_array.m
@@ -2,13 +2,13 @@ function [ values, s ] = closest_two_array( original, discovered )
 % CLOSEST_TWO_ARRAY Summary of this function goes here
 %   Detailed explanation goes here
 
-    values = zeros(length(original),2);
+    values = zeros(length(original),3);
     for i = 1 : length(original)
         current_value = original(i);
         
         distances = abs(discovered - current_value);
         [dist, index] = min(distances);
-        values(i,:) = [discovered(index) dist];
+        values(i,:) = [current_value discovered(index) dist];
     end
     s = sum(values(:,2))
 end
